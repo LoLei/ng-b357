11f1ed4becfb48bbc56bdbda2ea7f21b35d595a8
Lorenz Leitner <lorenz.leitner@ascent.io>
2025-05-09 14:09:37 +0200

    Add skip link

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { DOCUMENT, NgOptimizedImage } from '@angular/common';
 
 import { BlinkService } from '../shared/blink.service';
 
@@ -14,6 +14,12 @@ export class SidebarComponent {
   private readonly blinkService = inject(BlinkService);
   private readonly elementRef = inject(ElementRef);
   private readonly router = inject(Router);
+  private readonly document = inject(DOCUMENT);
+  private readonly focusableSelector = `
+    a[href], button:not([disabled]):not(.navbar-toggler), textarea:not([disabled]),
+    input:not([disabled]):not([type="hidden"]),
+    select:not([disabled]), [tabindex]:not([tabindex="-1"])
+  `;
 
   protected blink(): void {
     this.blinkService.blinkElementsFirstChild(this.elementRef);
@@ -21,5 +27,20 @@ export class SidebarComponent {
 
   protected goHome(): void {
     this.router.navigate(['/home']);
+  }
+  protected onSkipToMainContent(): void {
+    const mainElements = this.document.getElementsByTagName('main');
+    if (!mainElements.length) {
+      console.warn('No main element found');
+      return;
+    }
+
+    const focusableElements = mainElements[0].querySelectorAll<HTMLElement>(this.focusableSelector);
+    if (focusableElements.length) {
+      console.log(focusableElements);
+      focusableElements[0].focus();
+    } else {
+      console.warn('No focusable elements found in main');
+    }
   }
 }
